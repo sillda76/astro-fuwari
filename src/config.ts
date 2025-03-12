b4cc32153fa339f9868fd0598644d73f5d84b98f
sillda76 <sillda76@gmail.com>
2025-03-13 05:38:50 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,8 +7,8 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Home',
-  subtitle: 'SilldaのBlog',
+  title: 'SilldaのBlog',
+  subtitle: 'Home',
   lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
     hue: 360,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
